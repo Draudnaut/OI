83e3568d25f944aa795d15575ed58a2b616cb59d
henhimicons <kanberra354562007@gmail.com>
2018-03-31 19:30:00 +0800

    attempt to solve VIJOS 1554

---
+++ vijos/1554/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+const int maxn=1e5+7;
+using namespace std;
+int coin[maxn];
+int main()
+{
+    int n,T;
+    while (scanf("%d%d",&n,&T)==2){
+        for (int i=0;i<n;i++){
+            scanf("%d",&coin[i]);
+            coin[i]--;
+        }
+
+    }
+    return 0;
+}
