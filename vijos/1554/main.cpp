1331098e69f74499401bfb453757730041bbebdc
henhimicons <kanberra354562007@gmail.com>
2018-04-03 20:27:41 +0800

    modify the framework of solution of vijos1554

--- vijos/1554/main.cpp
+++ vijos/1554/main.cpp
@@ -1,17 +1,39 @@
 #include <bits/stdc++.h>
 const int maxn=1e5+7;
 using namespace std;
+int n;
+long long T;
 int coin[maxn];
+void quick_mul(){
+
+}
+void put_ans(int judge){
+    if (judge==1){
+        printf("0 %d",coin[0]+1);
+        for (int i=0;i<n;i++){
+            printf(" 0 %d",coin[i]+1);
+        }
+        puts("");
+    }else{
+        printf("%d 0",coin[0]+1);
+        for (int i=0;i<n;i++){
+            printf(" %d 0",coin[i]+1);
+        }
+        puts("");
+    }
+}
 int main()
-{
-    int n;
-    long long T;
+{    
     while (scanf("%d%lld",&n,&T)==2){
         for (int i=0;i<n;i++){
             scanf("%d",&coin[i]);
             coin[i]--;
         }
-        int change=0;
+        while (T){
+            if (T&1) quick_mul();
+            T>>=1;
+        }
+        put_ans(T&1);
     }
     return 0;
 }
